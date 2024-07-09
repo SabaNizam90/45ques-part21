e0ad848338174b8bf62b67d40d6a8fa900fc732d
Saba Nizam <160767347+SabaNizam90@users.noreply.github.com>
2024-07-10 00:10:25 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+//They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+var person = { name: "Saba Nizam", fname: "female", age: 26, Education: "Masters", City: "Karachi", Country: "Pakistan", Skills: "Currently selected in an IT course by Governor Sindh initiative" };
+console.log(person);
---
+++ main.ts
@@ -0,0 +1,4 @@
+//They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+let person: {name:string, fname:string, age:number, Education:string, City:String, Country:string, Skills:string} = {name:"Saba Nizam", fname:"female", age: 26, Education:"Masters", City:"Karachi", Country:"Pakistan", Skills:"Currently selected in an IT course by Governor Sindh initiative"};
+console.log(person);
